d0996ebcd298bb97e18cbc3b1d3e2d312b5a6a81
Emmanuel Jr <46963516+masterjayr@users.noreply.github.com>
2026-08-05 13:12:59 +0100

    Add: edit-distance - submission-2

---
+++ Data Structures & Algorithms/edit-distance/submission-2.py
@@ -0,0 +1,65 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        cache = {}
+
+        def dfs(i, j):
+            if i == len(word1):
+                return len(word2) - j 
+            if j == len(word2):
+                return len(word1) - i
+
+            if (i, j) in cache:
+                return cache[(i, j)]
+
+            if word1[i] == word2[j]:
+                cache[(i, j)] = dfs(i + 1, j + 1)
+                return cache[(i, j)]
+            else:
+                right = dfs(i, j+1)
+                down = dfs(i+1, j)
+                diagonal = dfs(i+1, j+1)
+                cache[(i,j)] = 1 + min(right, down, diagonal)
+
+                return cache[(i, j)]
+
+        # return dfs(0, 0)
+
+        # dp = [[float("inf")] * (len(word2) + 1) for i in range(len(word1) + 1)]
+        
+        dp = [float("inf")] * (len(word2) + 1)
+
+        # initialize base cases 
+        # for j in range(len(word2)+1):
+        #     dp[len(word1)][j] = len(word2) - j 
+        # for i in range(len(word1) + 1):
+        #     dp[i][len(word2)] = len(word1) - i
+
+        for j in range(len(word2) + 1):
+            dp[j] = len(word2) - j 
+
+        for i in range(len(word1)-1, -1, -1):
+            # new row init
+            nextDp = [float("inf")] * (len(word2) + 1)
+            nextDp[len(word2)] = len(word1) - i
+            for j in range(len(word2)-1, -1, -1):
+                if word1[i] == word2[j]:
+                    nextDp[j] = dp[j + 1]
+                else:
+                    right = nextDp[j+1]
+                    down = dp[j]
+                    diagonal = dp[j+1]
+                    nextDp[j] = 1 + min(right, down, diagonal)
+            dp = nextDp
+        return dp[0]
+
+        # for i in range(len(word1)-1, -1, -1):
+        #     for j in range(len(word2) -1, -1, -1):
+        #         if word1[i] == word2[j]:
+        #             dp[i][j] = dp[i + 1][j + 1]
+        #         else:
+        #             right = dp[i][j+1]
+        #             down = dp[i+1][j]
+        #             diagonal = dp[i+1][j+1]
+        #             dp[i][j] = 1 + min(right, down, diagonal)
+
+        # return dp[0][0]
