f652340b9f4e2c1e13f447a7ace5ef35ee0e5d04
Emmanuel Jr <46963516+masterjayr@users.noreply.github.com>
2026-08-20 15:46:02 +0100

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # so idea is x^2 => x^1 x x^1 so if n is odd say x^3 it's x * x^1 * x^1 
+
+        def helper(x, n):
+            if x == 0: return 0
+            if n == 0: return 1
+
+            res = helper(x, n // 2)
+            res = res * res
+            return x * res if n % 2 else res 
+
+        res = helper(x, abs(n))
+
+        return res if n >= 0 else 1 / res 
